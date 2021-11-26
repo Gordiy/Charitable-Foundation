f45f26ce78032d0eb71fcd9101d0f4f303628b7c
bodya1007 <bogdan21510@gmail.com>
2021-11-27 00:48:41 +0200

    Add : conteiner for display news info

---
+++ src/components/NewContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+ 
+const NewConteiner = (props)=>{
+    console.log(props);
+    return (
+        <div class="card shadow-sm">
+                        <img src={props.image} width="100%" height="225"/>
+
+
+        <div class="card-body">
+        {
+                      (props.togle)? <p>{props.mm.text}</p>
+                          :<p>{props.mm.text.slice(0,101)}...</p>
+                  }
+                 
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+            <button type="button" class="btn btn-sm btn-outline-secondary" onClick = {()=>props.settogle(!props.togle)}>{props.names}</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Долучитися</button>
+            </div>
+            <small class="text-muted">9 mins</small>
+        </div>
+        </div>
+    </div>
+           
+    )   
+}
+export default NewConteiner;
--- src/components/news.jsx
+++ src/components/news.jsx
@@ -1,60 +1,35 @@
-function News(props) {
+import React,{useState} from 'react'
+import NewConteiner from './NewContainer';
+function News({props}) {
+    const [togle,settogle] = useState(true)
+    let names;
+    let image="static/photo/action2.jpg"
+    let mm =[  
+        {
+            photo:"static/photo/action2.jpg",
+            text:` Ще у розпал карантину, наша команда ініціювала благодійний збір коштів на боротьбу з коронавірусом. Протягом місяця ми знаходили небайдужих людей, які підтримали б нашу ідею. Метою було купівля необхідного обладнання для Львівської обласної інфекційної лікарні.
+            За цей час нам вдалось зібрати трохи більше 48 тис. грн від майже півтори сотні благодійників😯`
+        }
+    ] 
+
+    if(togle)
+    {names= 'Сховати' }
+    else {names = 'Переглянути'}
     return (
         <div class="album py-5 bg-light">
             <div class="container">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                     <div class="col">
-                        <div class="card shadow-sm">
-                        <img src="static/photo/action2.jpg" width="100%" height="225" />
-                            <div class="card-body">
-                                <p class="card-text">
-                                    Ще у розпал карантину, наша команда ініціювала благодійний збір коштів на боротьбу з коронавірусом. Протягом місяця ми знаходили небайдужих людей, які підтримали б нашу ідею. Метою було купівля необхідного обладнання для Львівської обласної інфекційної лікарні.
-                                    За цей час нам вдалось зібрати трохи більше 48 тис. грн від майже півтори сотні благодійників😯
-                                </p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Переглянути</button>
-                                    <button type="button" class="btn btn-sm btn-outline-secondary">Долучитися</button>
-                                    </div>
-                                    <small class="text-muted">9 mins</small>
-                                </div>
-                            </div>
-                        </div>
+                 
+                        <NewConteiner props = {props} names={names} mm={mm[0]} togle={togle} settogle = {settogle} image={image} />
+                  
                     </div>
-
                     <div class="col">
-                        <div class="card shadow-sm">
-                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                            <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Переглянути</button>
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Долучитися</button>
-                                </div>
-                                <small class="text-muted">9 mins</small>
-                            </div>
-                            </div>
-                        </div>
+                      
                     </div>
+                   
                     
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                            <div class="card-body">
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Переглянути</button>
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Долучитися</button>
-                                </div>
-                                <small class="text-muted">9 mins</small>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
         </div>

d824a921a2421e3909286a736e393734bd8795d4
hordii.rushynets <hordii.rushynets@globallogic.com>
2021-11-26 19:36:59 +0200

    Add: create jsx header using react.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import SiteHeader from './components/header'
 
 function App() {
-  return "hello"
+  let info = {
+    contacts: [
+		{
+			url: "https://www.facebook.com/groups/626423081826328/?ref=share",
+			text: "Слідкуйте за нами Facebook"
+		}, 
+		{
+			url: "https://www.instagram.com/plich_o_plich/",
+			text: "Вподобайте Instagram"
+		}, 
+		{
+			url: "gord.rushynes@gmail.com",
+			text: "Пишіть на Email"
+		}
+	],
+    about: "Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information."
+  }
+  return <SiteHeader about={info.about} contacts={info.contacts}/>
 }
 
 export default App;
